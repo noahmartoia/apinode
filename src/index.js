33b00919f1188e29ce81546a198a2e24212a7948
noahmartoia <noah.martoia@ecole-isitech.fr>
2024-04-09 15:27:53 +0200

    auth feat

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 import "dotenv/config";
 import express from "express";
 import carsRoutes from "./routes/cars.js";
+import authRoutes from "./routes/auth.js";
+import mongoose from "mongoose";
+import { handleUncaughtErrors } from "./Middlewares/error.js";
+import isAuth from "./Middlewares/auth.js";
 
 const app = express();
 
-console.log('env: ', process.env.MONGO_STRING)
-
 const PORT = process.env.PORT || 3001;
+const MONGO_STRING = process.env.MONGO_STRING;
 
 // Middlewares
 // Middleware qui permet de parser les donnees issues d'un formulaire
 app.use(express.json());
 
-app.use("/cars", carsRoutes);
+app.use("/cars", isAuth, carsRoutes);
+app.use("/auth", authRoutes);
 
-/* 
- Routes definit une route Get sur / qui renvoie un message
- La fonction app.get prend deux arguments
- le chemin de la route
- et une fonction qui prend deux arguments :
- request et response 
- */
-app.get("/", (request, response) => {
-  response.json({ message: "Hello World!" });
-  // JSON : Javascript Object Notation
+app.use("/error", (req, res) => {
+  try {
+    //
+    throw new Error("This is an error");
+  } catch (error) {}
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Middleware pour gerer les erreurs
+app.use(handleUncaughtErrors);
+
+mongoose.connect(MONGO_STRING).then(() => {
+  console.log("Connected to the database!");
+
+  // On lance le serveur
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
--- src/routes/cars.js
+++ src/routes/cars.js
@@ -1,82 +1,42 @@
 import express from "express";
+import {
+  createCar,
+  deleteCar,
+  getCar,
+  getCars,
+  getError,
+  udpateCar,
+} from "../controller/cars.js";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-let mockCars = [
-  { id: 1, brand: "Audi", model: "A3" },
-  { id: 2, brand: "Renault", model: "Clio" },
-  { id: 3, brand: "Peugeot", model: "208" },
-  { id: 4, brand: "BMW", model: "Serie 1" },
-  { id: 5, brand: "Mercedes", model: "Classe A" },
-  { id: 6, brand: "Citroen", model: "C3" },
-  { id: 7, brand: "Ford", model: "Fiesta" },
-  { id: 8, brand: "Opel", model: "Corsa" },
-  { id: 9, brand: "Toyota", model: "Yaris" },
-  { id: 10, brand: "Volkswagen", model: "Polo" },
-];
-
 // GET http://localhost:3001/cars
-router.get("/", (request, response) => {
-  response.json(mockCars);
-});
+router.get("/", getCars);
 
 // GET http://localhost:3001/cars/1
-router.get("/:id", (request, response) => {
-  const id = parseInt(request.params.id, 10);
-  console.log(id);
-  if (isNaN(id)) {
-    response.status(400).json({ message: "Invalid id" });
-  }
-  const car = mockCars.find((car) => car.id === id);
-  if (car) {
-    response.json(car);
-  } else {
-    response.status(404).json({ message: "Car not found" });
-  }
-});
+router.get("/:id", getCar);
 
 // POST http://localhost:3001/cars
-router.post("/", (request, response) => {
-  const bodyContent = request.body;
-  const id = mockCars.length + 1;
-  const newCar = { id, ...bodyContent };
-  mockCars.push(newCar);
-  response.status(201).json(newCar);
-});
+router.post(
+  "/",
+  [
+    body("brand").trim().isLength({ max: 20, min: 2 }),
+    body("model").trim().isLength({ min: 2, max: 100 }),
+  ],
+  createCar
+);
 
 // PUT http://localhost:3001/cars/1 creer une route qui
 // permet de modiier une voiture
-router.put("/modify/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (isNaN(id)) {
-    res.status(400).json({ message: "Invalid id" });
-  }
-  const car = mockCars.find((car) => car.id === id);
-  if (car) {
-    const bodyContent = req.body;
-    mockCars[id-1]={
-      id : id,
-      brand : bodyContent.brand,
-      model : bodyContent.model,
-    }
-    res.status(201).json(mockCars);
-  } else {
-    res.status(404).json({ message: "Car not found" });
-  }
-});
+router.put("/:id", udpateCar);
+
 // DELETE http://localhost:3001/cars/1 creer une route qui
 // permet de supprimer une voiture
+router.delete("/:id", deleteCar);
 
-router.delete("/:id", (request, response) => {
-  const id = parseInt(request.params.id, 10);
-  const car = mockCars.find((car) => car.id === id);
-  if (car) {
-    mockCars = mockCars.filter((car) => car.id !== id);
-    response.status(201).json(mockCars);
-  } else {
-    response.status(404).json({ message: "Car not found" });
-  }
-});
+// ERROR
+router.get("/error", getError);
 
 // Ceci est un export default, on peut en avoir
 // qu'un seul par fichier (module)
